fff7b973c19ba979c07173fba70ed9f39b700440
jordonias <jordonias@jordonias-desktop.(none)>
2016-01-18 18:43:28 -0600

    basic api

---
+++ index.js
@@ -0,0 +1,93 @@
+/**
+ * Module dependencies
+ */
+
+var _ = require('lodash');
+var Client = require('node-rest-client').Client;
+var http = require('http');
+var makeUrl = require('./lib/makeUrl');
+var resources = require('./lib/resources');
+
+/**
+ * Module variables
+ */
+
+var client = new Client();
+
+/**
+ * 
+ */
+
+_.map(resources, function(resource) {
+  if(typeof resource.method !== 'undefined') {
+    exports[resource.name] = function(params, query, cb) {
+      var url;
+      if(resource.method === 'POST' || resource.method === 'PUT') {
+        url = makeUrl(resource.path, params);
+      } else {
+        url = makeUrl(resource.path, params, query);
+      }
+
+      var req = http.request({
+        hostname: 'localhost',
+        port: 18469,
+        method: resource.method,
+        path: url
+      }, function(res) {
+        res.on('data', function(chunk) {
+          console.log(chunk.toString());
+        });
+
+        res.on('error', function(err) {
+          console.log(err);
+        });
+
+        res.on('end', function() {
+          console.log('end'); 
+        });
+      });
+
+      if(resource.method === 'POST' || resource.method === 'PUT') {
+        req.write(query);
+      }
+
+      req.end();
+    }
+  } else {
+    exports[resource.name] = _.mapValues(resource.methods, function(x) {
+      return function(params, query, cb) {
+        var url;
+        if(x.method === 'POST' || x.method === 'PUT') {
+          url = makeUrl(x.path, params);
+        } else {
+          url = makeUrl(x.path, params, query);
+        }
+
+        var req = http.request({
+          hostname: 'localhost',
+          port: 18469,
+          method: x.method,
+          path: url
+        }, function(res) {
+          res.on('data', function(chunk) {
+            console.log(chunk.toString());
+          });
+
+          res.on('error', function(err) {
+            console.log(err);
+          });
+
+          res.on('end', function() {
+            console.log('end'); 
+          });
+        });
+
+        if(x.method === 'POST' || x.method === 'PUT') {
+          req.write(query);
+        }
+
+        req.end();
+      }
+    });
+  }
+});
